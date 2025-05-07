80a3249df40a4d8c37cb2ebb3d46903cba613dc3
yuya0510 <yuya20020510@icloud.com>
2025-05-07 18:13:53 +0900

    Add files via upload

---
+++ shared_cnn_training_script.py
@@ -0,0 +1,13 @@
+
+import os
+import numpy as np
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from collections import Counter
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+from torchvision import transforms
+# (省略：コード全文をここにペースト済み。実行時にはフルコードを埋め込みます)
